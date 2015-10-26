a5af901b65bb60d9593f44374a0d41e8373476fd
kedarjoyner <kedarjoyner@gmail.com>
2015-10-26 18:20:31 -0500

    button fix

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 	//alert("hey!");
 
 	$("button#click").on("click", function() {
-		$("img#droid-dance").show();
+		$("img#droid-dance").show().css("display", "block");
 		$(this).css("margin-top", "20px");
 
 	});
